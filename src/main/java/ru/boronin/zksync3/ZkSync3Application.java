aa38682241ab0022d8acb7e1fc6d596245c0d0f9
Bpar_Hapoda <cddvdrom@yandex.ru>
2023-10-11 21:18:30 +0300

    First Commit

--- src/main/java/ru/boronin/zksync3/ZkSync3Application.java
+++ src/main/java/ru/boronin/zksync3/ZkSync3Application.java
@@ -1,13 +1,21 @@
 package ru.boronin.zksync3;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ZkSync3Application {
+    static final Logger log =
+            LoggerFactory.getLogger(ZkSync3Application.class);
 
-	public static void main(String[] args) {
-		SpringApplication.run(ZkSync3Application.class, args);
-	}
+    public static void main(String[] args) {
 
+        SpringApplication.run(ZkSync3Application.class, args);
+
+        log.info("Перед запуском приложения ");
+        log.debug("Starting my application in debug with {} args", args.length);
+        log.info("Starting my application with {} args.", args.length);
+    }
 }
--- src/main/java/ru/boronin/zksync3/controllers/UserController.java
+++ src/main/java/ru/boronin/zksync3/controllers/UserController.java
@@ -1,2 +1,36 @@
-package ru.boronin.zksync3.controllers;public class UserController {
+package ru.boronin.zksync3.controllers;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import ru.boronin.zksync3.ZkSync3Application;
+
+import java.io.BufferedOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+
+@Controller
+public class UserController {
+    static final Logger log =
+            LoggerFactory.getLogger(ZkSync3Application.class);
+    @GetMapping("/")
+    public String index (HttpServletRequest request) throws IOException {
+        log.info("["+request.getRemoteAddr()+request.getMethod()+request.getHeaders("HTTP_USER_AGENT")+"]");
+   //     logger.info("Info message ....................................................");
+
+        Socket socket = new Socket("62.217.179.229", 5044);
+        DataOutputStream os = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
+        os.writeBytes("{\"username\": {\"SurName\":\"Boronin\"} }");
+        os.flush();
+        socket.close();
+
+
+
+        return "index";
+    }
 }
